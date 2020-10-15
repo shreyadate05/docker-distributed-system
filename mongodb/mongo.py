2049e59ffbd7a533f94831dcb25d6a9279e9d588
shreyadate05 <shreyadate05@gmail.com>
2020-10-15 15:54:03 -0400

    taskdb ready

--- mongodb/mongo.py
+++ mongodb/mongo.py
@@ -5,24 +5,21 @@ mongodb_uri = os.getenv('MONGODB_URI', default='mongodb://localhost:27017/')
 client = pymongo.MongoClient(mongodb_uri)
 
 #client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["customersdb"]
-customers = db["customers"]
-customers_list = [
-  { "name": "Amy", "address": "Apple st 652"},
-  { "name": "Hannah", "address": "Mountain 21"},
-  { "name": "Michael", "address": "Valley 345"},
-  { "name": "Sandy", "address": "Ocean blvd 2"},
-  { "name": "Betty", "address": "Green Grass 1"},
-  { "name": "Richard", "address": "Sky st 331"},
-  { "name": "Susan", "address": "One way 98"},
-  { "name": "Vicky", "address": "Yellow Garden 2"},
-  { "name": "Ben", "address": "Park Lane 38"},
-  { "name": "William", "address": "Central st 954"},
-  { "name": "Chuck", "address": "Main Road 989"},
-  { "name": "Viola", "address": "Sideway 1633"}
-]
-x = customers.insert_many(customers_list)
-# print list of the _id values of the inserted documents:
+db = client["TasksDB"]
+tasks = db["tasks"]
+tasks_list = []
+
+for n in range(0, 100):
+    task = {}
+    task["taskname"] = "task " + str(n+1)
+    task["sleeptime"] = 60
+    task["state"] = "created"
+    task["host"] = "slave " + str(n+1)
+    tasks_list.append(dict(task))
+
+
+
+x = tasks.insert_many(tasks_list)
 print(x.inserted_ids)
-for x in customers.find():
+for x in tasks.find():
     print(x)
