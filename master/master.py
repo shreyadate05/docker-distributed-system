83549a4a634aa010448e7adbc897643b50af7fd4
shreyadate05 <shreyadate05@gmail.com>
2020-10-16 18:27:08 -0400

    10 slaves connected

--- master/master.py
+++ master/master.py
@@ -5,18 +5,32 @@
 # You don't have to install anything special, these libraries are installed with Python.
 import os
 import pymongo
-import http.server
-import socketserver
-
+import socket
 
 print("[MASTER] Hi!")
 
-client = pymongo.MongoClient("mongodb://root:example@mongo:27017")
-db = client["TasksDB"]
-print("[MASTER] DB is: ", db)
+def connectToMongo():
+    client = pymongo.MongoClient("mongodb://root:example@mongo:27017")
+    db = client["TasksDB"]
+    print("[MASTER] DB is: ", db)
 
+def server_program():
+    host = socket.gethostname()
+    port = 2345
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(2)
+    conn, address = server_socket.accept()
+    print("Connection from: " + str(address))
+    while True:
+        data = conn.recv(1024).decode()
+        print("[MASTER] ", data)
+        if not data:
+            break
+        data = "Hi from Server"
+        conn.send(data.encode())
+    conn.close()
 
-handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", 1234), handler) as httpd:
-    httpd.serve_forever()
-
+connectToMongo()
+while True:
+    server_program()
--- slave/slave.py
+++ slave/slave.py
@@ -1,11 +1,18 @@
-import urllib.request
+import socket
+import time
+
+def client_program():
+    host = "master"
+    port = 2345
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+    data = "Hi from Client"
+    client_socket.send(data.encode())
+    data = client_socket.recv(1024).decode()
+    print('Received from server: ' + data)
+    client_socket.close()
 
 
-print("Hi from client")
-fp = urllib.request.urlopen("http://localhost:1234/")
-
-print(fp)
-
-
-
-fp.close()
+if __name__ == '__main__':
+    time.sleep(5)
+    client_program()
